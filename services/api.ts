1fb61d528856092c8a9830e8e9b35cdfbf46c2ec
Hanish Rishen <110720727+hanish-rishen@users.noreply.github.com>
2025-04-16 14:52:18 +0530

    Updated for Cloudflare

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://uaqmp-api.hanishrishen.workers.dev/api";
 
 export interface AirQualityData {
   timestamp: number;
